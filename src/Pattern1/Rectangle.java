2ddff3f68d934556c37270168ba3ea4f7c3daba2
Pooja <gujalwarpooja@gmail.com>
2023-11-05 21:07:25 +0100

    New rectangle class is added

---
+++ src/Pattern1/Rectangle.java
@@ -0,0 +1,10 @@
+package Pattern1;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("********");
+        System.out.println("********");
+        System.out.println("You can now print new shape");
+    }
+}
