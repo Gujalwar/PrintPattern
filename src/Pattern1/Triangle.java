762510f18f6ddc7eaed35ef46b6b894a63ea6065
Pooja <gujalwarpooja@gmail.com>
2023-11-05 12:38:25 +0100

    New Triangle class is uploaded

---
+++ src/Pattern1/Triangle.java
@@ -0,0 +1,13 @@
+package Pattern1;
+
+public class Triangle {
+    public static void main(String[] args) {
+        System.out.println("  *");
+        System.out.println(" ***");
+        System.out.println("*****");
+        System.out.println("Triangle is complete");
+        System.out.println("You can create a new shape again");
+
+    }
+}
+
