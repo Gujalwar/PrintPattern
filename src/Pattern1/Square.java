15916ba08cf8283f5ec7b7f51cb58d2e38adcb22
Pooja <gujalwarpooja@gmail.com>
2023-11-05 14:56:58 +0100

    Square class is added

---
+++ src/Pattern1/Square.java
@@ -0,0 +1,11 @@
+package Pattern1;
+
+public class Square {
+    public static void main(String[] args) {
+        System.out.println("******");
+        System.out.println("******");
+        System.out.println("******");
+        System.out.println("Square is complete");
+        System.out.println("You can add new shappe");
+    }
+}
